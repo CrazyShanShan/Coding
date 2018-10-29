ea715109e38f750c770a129d05b4049e7c29e61a
CrazyShanShan <824375721@qq.com>
2018-10-29 10:27:12 +0800

    -m "HDU2054修改（大数）"

--- src/hdu2000-2099/HDU_2054.cpp
+++ src/hdu2000-2099/HDU_2054.cpp
@@ -1,14 +1,32 @@
 #include<cstdio>
 #include<algorithm>
+#include<cstring>
 using namespace std;
-
+const int maxn = 1000000 + 10;
+char a[maxn], b[maxn];
+void change(char* s) {
+	int len = strlen(s);
+	if (strstr(s, "."))
+	{
+		for (int i = len - 1; s[i] == '0'; i--) {
+			s[i] = '\0';
+			len--;
+		}
+	}
+	if (s[len - 1] == '.') {
+		s[len - 1] = '\0';
+	}
+}
 int main()
 {
-    double a,b;
-    while(scanf("%lf%lf",&a,&b) == 2)
-    {
-        if(a==b) printf("YES\n");
-        else printf("NO\n");
-    }
-    return 0;
+	while (scanf("%s%s", &a, &b) == 2)
+	{
+		change(a);
+		change(b);
+		if (strcmp(a, b) == 0) {
+			printf("YES\n");
+		}
+		else printf("NO\n");
+	}
+	return 0;
 }
